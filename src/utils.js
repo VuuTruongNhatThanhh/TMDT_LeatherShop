d236fba00aedd8757713ed4f8cb1ee836aef3fe9
KaAnThienPhuc <116794491+KaAnThienPhuc@users.noreply.github.com>
2024-05-10 11:51:12 +0700

    getbase64 avt

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,13 @@ export const isJsonString = (data) => {
    
     return true
 }
+
+//Đưa file nhận vào thành một chuỗi base64 (file avatar)
+// CHuỗi base64 được lưu trong mongoDB sẽ có thể biến lại thành hình ảnh
+export const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
