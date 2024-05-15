bb36ce0b681b8343fa4c49bc051c8493db83a4ac
Nguyễn Đình Lưu <131143370+NguyenDinhLuu211@users.noreply.github.com>
2024-05-15 16:36:07 +0700

    ProductService to manage product

---
+++ src/services/ProductService.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+import API_URL_BACK_END from "../apiConfig";
+import { axiosJWT } from "./UserService";
+export const getAllProduct = async () => {
+    const res = await axios.get(`${API_URL_BACK_END}/product/get-all`)
+    return res.data
+}
+
+export const createProduct = async (data) => {
+    const res = await axios.post(`${API_URL_BACK_END}/product/create`, data)
+    return res.data
+}
+
+export const getDetailsProduct = async (id) => {
+    const res = await axios.get(`${API_URL_BACK_END}/product/get-details/${id}`)
+    return res.data
+}
+
+export const updateProduct = async (id, access_token, data) => {
+    const res = await axiosJWT.put(`${API_URL_BACK_END}/product/update/${id}`,data,{
+        headers: {
+            token:`Bearer ${access_token}`, 
+        }
+    })
+    return res.data
+}
+
+export const deleteProduct = async (id, access_token) => {
+    const res = await axiosJWT.delete(`${API_URL_BACK_END}/product/delete/${id}`,{
+        headers: {
+            token:`Bearer ${access_token}`, 
+        }
+    })
+    return res.data
+}
