d0ad38718e6d18612480046d7004220fc9011506
VuuTruongNhatThanhh <116423300+VuuTruongNhatThanhh@users.noreply.github.com>
2024-06-16 16:06:32 +0700

    update link backend

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 // apiConfig.js
-const API_URL_BACK_END = "http://localhost:3000/api"; // Đặt URL API của bạn ở đây
+const API_URL_BACK_END = "https://tmdt-leathershop-backend.onrender.com"; // Đặt URL API của bạn ở đây
 const REACT_APP_FB_ID="1174192080256009";
-const REACT_APP_IS_LOCAL=true;
+const REACT_APP_IS_LOCAL=false;
 
 export { API_URL_BACK_END, REACT_APP_FB_ID, REACT_APP_IS_LOCAL};
